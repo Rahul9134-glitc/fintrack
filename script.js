d11ff348f161735be5a2bf42dadd5135e30390e4
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2026-07-03 22:50:19 +0530

    updated my finstrack project

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let records = JSON.parse(localStorage.getItem("fintrack_logs")) || [];
 let selectedCurrency = localStorage.getItem("fintrack_currency_code") || "INR";
 let activeFilter = "all";
 let flowChart = null;
+let editingId = null;
+
+let searchText = ""
+
 
 const currencyMap = {
   INR: "₹",
@@ -10,7 +14,6 @@ const currencyMap = {
   GBP: "£",
   JPY: "¥",
 };
-
 
 const balanceElement = document.getElementById("bal-value");
 const incomeElement = document.getElementById("inc-value");
@@ -44,15 +47,23 @@ function processCardCalculations() {
 function renderTransactionsTable() {
   tableBodyElement.innerHTML = "";
   const filteredRecords = records.filter((item) => {
-    if (activeFilter === "income") {
-      return item.type === "income";
-    }
-    if (activeFilter === "expense") {
-      return item.type === "expense";
-    } else {
-      return true;
-    }
-  });
+
+        if (activeFilter !== "all" && item.type !== activeFilter) {
+            return false;
+        }
+
+        if (searchText === "") {
+            return true;
+        }
+
+        return (
+            item.description.toLowerCase().includes(searchText) ||
+            item.category.toLowerCase().includes(searchText) ||
+            item.type.toLowerCase().includes(searchText) ||
+            item.amount.toString().includes(searchText) ||
+            item.date.includes(searchText)
+        );
+    });
 
   if (filteredRecords.length === 0) {
     tableBodyElement.innerHTML = `<tr><td colspan="5" style="text-align:center; color: var(--text-muted);">No records logged yet.</td></tr>`;
@@ -76,10 +87,19 @@ function renderTransactionsTable() {
             <td style="font-weight: 500;">${item.description}</td>
             <td><span style="background: var(--bg-global); padding: 4px 8px; border-radius: 4px; font-size:12px;">${item.category}</span></td>
             <td class="${typeStyle}">${prefix}${symbol}${parseFloat(item.amount).toFixed(2)}</td>
-            <td>
-                <!-- Delete button me item.id pass kar rahe hain taaki JS ko pata chale kaunsa delete karna hai -->
-                <button class="row-delete-btn" onclick="eraseRecord('${item.id}')">Delete</button>
-            </td>
+           <td>
+    <button
+        class="row-edit-btn"
+        onclick="editRecord('${item.id}')">
+        Edit
+    </button>
+
+    <button
+        class="row-delete-btn"
+        onclick="eraseRecord('${item.id}')">
+        Delete
+    </button>
+</td>
         `;
 
       tableBodyElement.appendChild(tr);
@@ -94,24 +114,36 @@ function processForm(event) {
   const date = document.getElementById("form-date").value;
   const category = document.getElementById("form-category").value;
 
-  const newLog = {
-    id: "tx_uid_" + Date.now(), //maine isame extra string jod kar ek accha id ek unique format jaisa use kiya hu sir
-    type,
-    description,
-    amount,
-    date,
-    category,
-  };
-
-  records.push(newLog); //maine apne records ke arrya me push kar diya hu.....
-
-  //yaha maine jaise padha hai jaise ki json Stringify jsaon data ko string me covert karta hai
+  if (editingId) {
+    const record = records.find((value) => value.id === editingId);
+    const index = records.findIndex((item) => item.id === editingId);
+    if (index !== -1) {
+      records[index] = {
+        id: editingId,
+        type,
+        description,
+        amount,
+        date,
+        category,
+      };
+      editingId = null;
+      showToast("Edit Transaction Sucessfully!")
+    }
+    document.getElementById("modal-title").innerHTML = "Add Transaction";
+  } else {
+    const newLog = {
+      id: "tx_uid_" + Date.now(),
+      type,
+      description,
+      amount,
+      date,
+      category,
+    };
+    records.push(newLog);
+    showToast("Add Transactions Sucessfully!")
+  }
   localStorage.setItem("fintrack_logs", JSON.stringify(records));
-
-  // 6. Modal ko band karna aur form ko reset (safaya) karna
   closeModal();
-
-  // 7. Golden Rule: Data badla toh UI badlo (Dono functions ko fir se chalana)
   masterRefresh();
 }
 
@@ -184,6 +216,8 @@ function closeModal() {
   if (form) {
     form.reset();
   }
+
+  editingId = null;
 }
 
 function handleFilterChange(value) {
@@ -203,24 +237,23 @@ function triggerReset() {
 }
 
 function saveSystemSettings() {
+  const username = document.getElementById("settings-name-input").value.trim();
 
-    const username =
-    document.getElementById("settings-name-input").value.trim();
+  const currency = document.getElementById("settings-currency-select").value;
 
-    const currency =
-    document.getElementById("settings-currency-select").value;
+  localStorage.setItem("fintrack_username", username);
 
-    localStorage.setItem("fintrack_username", username);
+  localStorage.setItem("fintrack_currency_code", currency);
 
-    localStorage.setItem("fintrack_currency_code", currency);
+  document.getElementById("display-username").innerText = username;
 
-    document.getElementById("display-username").innerText = username;
+  selectedCurrency = currency;
 
-    selectedCurrency = currency;
+  masterRefresh();
 
-    masterRefresh();
+  showToast("Settings Saved Successfully");
 
-    alert("Settings Saved Successfully");
+  changePage("dashboard")
 }
 
 function masterRefresh() {
@@ -246,7 +279,6 @@ function changePage(targetPage) {
     btnDash.classList.remove("active");
     btnSett.classList.add("active");
   }
-
 }
 
 function handleThemeToggle(isDark) {
@@ -292,7 +324,7 @@ function registerUser(event) {
 
   localStorage.setItem("fintrack_user", JSON.stringify(user));
 
-  alert("Registration Successful");
+  showToast("Register Sucesfully!")
 
   document.getElementById("register-form").reset();
 
@@ -308,25 +340,24 @@ function loginUser(event) {
 
   const savedUser = JSON.parse(localStorage.getItem("fintrack_user"));
 
-   document.getElementById("settings-name-input").value =
-   savedUser.name;
+  document.getElementById("settings-name-input").value = savedUser.name;
 
   if (!savedUser) {
-    alert("Please Register First");
+    showToast("Please Register First");
 
     return;
   }
 
   if (savedUser.email === email && savedUser.password === password) {
-    alert("Login Successful");
+    showToast("Login Successful");
     document.getElementById("auth-container").classList.add("hidden");
 
     document.querySelector(".app-container").classList.remove("hidden");
 
-    const username  = document.getElementById("display-username").innerText = savedUser.name;
-    console.log(username)
-    document.getElementById("settings-name-input").value =
-    savedUser.name;
+    const username = (document.getElementById("display-username").innerText =
+      savedUser.name);
+    console.log(username);
+    document.getElementById("settings-name-input").value = savedUser.name;
 
     localStorage.setItem("fintrack_loggedIn", "true");
   } else {
@@ -335,73 +366,98 @@ function loginUser(event) {
 }
 
 function logoutUser() {
-
   if (!confirm("Are you sure you want to logout?")) {
     return;
   }
 
-  document
-    .getElementById("auth-container")
-    .classList.remove("hidden");
+  document.getElementById("auth-container").classList.remove("hidden");
 
-  document
-    .querySelector(".app-container")
-    .classList.add("hidden");
+  document.querySelector(".app-container").classList.add("hidden");
 
-  document
-    .getElementById("login-form")
-    .reset();
+  document.getElementById("login-form").reset();
 
   localStorage.removeItem("fintrack_loggedIn");
 }
 
-
 window.onload = function () {
-
   const isLoggedIn = localStorage.getItem("fintrack_loggedIn");
   const user = JSON.parse(localStorage.getItem("fintrack_user"));
 
   if (isLoggedIn === "true" && user) {
-
     document.getElementById("auth-container").classList.add("hidden");
     document.querySelector(".app-container").classList.remove("hidden");
     document.getElementById("display-username").innerText = user.name;
-
   } else {
-
     document.getElementById("auth-container").classList.remove("hidden");
     document.querySelector(".app-container").classList.add("hidden");
-
   }
-
 
   const savedName = localStorage.getItem("fintrack_username");
 
   if (savedName) {
-
     document.getElementById("display-username").innerText = savedName;
 
     document.getElementById("settings-name-input").value = savedName;
-
   }
 
   const savedCurrency = localStorage.getItem("fintrack_currency_code");
 
   if (savedCurrency) {
-
     selectedCurrency = savedCurrency;
 
     document.getElementById("settings-currency-select").value = savedCurrency;
-
   }
 
   const dark = localStorage.getItem("fintrack_dark");
 
   if (dark === "true") {
-
     document.body.classList.add("dark");
 
     document.getElementById("dark-mode-toggle").checked = true;
   }
   masterRefresh();
+};
+
+function editRecord(targetId) {
+  const record = records.find((item) => item.id === targetId);
+  ;
+
+  if (!record) return;
+
+  editingId = targetId;
+  document.getElementById("modal-title").innerText = "Edit Transaction"
+
+  document.getElementById("form-type").value = record.type;
+
+  document.getElementById("form-desc").value = record.description;
+
+  document.getElementById("form-amount").value = record.amount;
+
+  document.getElementById("form-date").value = record.date;
+
+  document.getElementById("form-category").value = record.category;
+
+  openModal();
+}
+
+function showToast(message, color = "#16a34a") {
+
+    Toastify({
+        text: message,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        close: true,
+        stopOnFocus: true,
+        style: {
+            background: color,
+            borderRadius: "8px"
+        }
+    }).showToast();
+
+}
+
+function handleSearch(value){
+  searchText = value.toLowerCase();
+  renderTransactionsTable()
 }
